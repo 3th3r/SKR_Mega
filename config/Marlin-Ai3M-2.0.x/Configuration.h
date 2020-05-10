0e4ac16097c6cca37aea587570514e6dc18f0be2
Ugo <u.sohier@gmail.com>
2020-05-10 20:46:52 +0200

    Activating linear advance

--- config/Marlin-Ai3M-2.0.x/Configuration.h
+++ config/Marlin-Ai3M-2.0.x/Configuration.h
@@ -735,6 +735,7 @@
  *                                      X, Y, Z, E0 [, E1[, E2...]]
  */
 #define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 400, 92.6 }
+
 /**
  * Default Max Feed Rate (mm/s)
  * Override with M203
@@ -753,7 +754,7 @@
  * Override with M201
  *                                      X, Y, Z, E0 [, E1[, E2...]]
  */
-#define DEFAULT_MAX_ACCELERATION      { 3000, 2000,  60, 10000 }
+#define DEFAULT_MAX_ACCELERATION      { 3000, 2000, 60, 10000 }
 
 //#define LIMITED_MAX_ACCEL_EDITING     // Limit edit via M201 or LCD to DEFAULT_MAX_ACCELERATION * 2
 #if ENABLED(LIMITED_MAX_ACCEL_EDITING)
