38e143b77cd8b679e516bb77c0b5a9875630e9fa
Ugo <u.sohier@gmail.com>
2020-05-16 15:04:32 +0200

    edit mesh bed, 5x5 mesh, 0.020mm step, activated menue

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1311,7 +1311,7 @@
 
   #define MESH_INSET 10          // Set Mesh bounds as an inset region of the bed
   #define GRID_MAX_POINTS_X 5    // Don't use more than 7 points per axis, implementation limited.
-  #define GRID_MAX_POINTS_Y 3    //  GRID_MAX_POINTS_X <= default 
+  #define GRID_MAX_POINTS_Y 5    //  GRID_MAX_POINTS_X <= default 
 
   //#define MESH_G28_REST_ORIGIN // After homing all axes ('G28' or 'G28 XYZ') rest Z at Z_MIN_POS
 
@@ -1326,7 +1326,7 @@
 #if ENABLED(LCD_BED_LEVELING)
   #define MESH_EDIT_Z_STEP  0.020 // (mm) Step size while manually probing Z axis.
   #define LCD_PROBE_Z_RANGE 4     // (mm) Z Range centered on Z_MIN_POS for LCD Z adjustment
-  //#define MESH_EDIT_MENU        // Add a menu to edit mesh points
+  #define MESH_EDIT_MENU        // Add a menu to edit mesh points
 #endif
 
 // Add a menu item to move between bed corners for manual bed adjustment
