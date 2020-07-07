2af6e5344afea4be2aef32b71844d8a62058807a
Ugo <u.sohier@gmail.com>
2020-07-07 10:54:52 +0200

    increase feerate for extruder E1

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -747,7 +747,7 @@
  * Override with M203
  *                                      X, Y, Z, E0 [, E1[, E2...]]
  */
-#define DEFAULT_MAX_FEEDRATE          { 500, 500, 6, 25 }
+#define DEFAULT_MAX_FEEDRATE          { 500, 500, 6, 75 }
 
 //#define LIMITED_MAX_FR_EDITING        // Limit edit via M203 or LCD to DEFAULT_MAX_FEEDRATE * 2
 #if ENABLED(LIMITED_MAX_FR_EDITING)
