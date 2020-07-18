07622ec25407005a05badf24366d37081b433741
Ugo <u.sohier@gmail.com>
2020-07-18 18:52:44 +0200

    Mesh Bed leveling, revert grid point to 5x5

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1310,8 +1310,8 @@
   //===========================================================================
 
   #define MESH_INSET 10          // Set Mesh bounds as an inset region of the bed
-  #define GRID_MAX_POINTS_X 3    // Don't use more than 7 points per axis, implementation limited.
-  #define GRID_MAX_POINTS_Y 3    //  GRID_MAX_POINTS_X <= default 
+  #define GRID_MAX_POINTS_X 5    // Don't use more than 7 points per axis, implementation limited.
+  #define GRID_MAX_POINTS_Y 5    //  GRID_MAX_POINTS_X <= default 
 
   //#define MESH_G28_REST_ORIGIN // After homing all axes ('G28' or 'G28 XYZ') rest Z at Z_MIN_POS
 
